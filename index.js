d29e825da0d682926b7998dfe74c16bbbe5b2098
Sneh Koul <snehkoul1999@gmail.com>
2019-04-15 12:50:05 -0700

    to open the original page

--- index.js
+++ index.js
@@ -10,7 +10,17 @@
 {
   // Do we trust the sender of this message?  (might be
   // different from what we originally opened, for example).
-  console.log(event.data)
+  if(event.data==='onboardingcomplete'){
+    var string = window.location.href
+    if( string.includes('fwd.metamask.io/')){
+      
+      var res = string.substring(17);
+      window.open(res);
+
+      window.close()
+    
+    }
+  }
 
   // event.source is popup
   // event.data is "hi there yourself!  the secret response is: rheeeeet!"
