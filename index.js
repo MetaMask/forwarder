3357e107e618359a4f73df0d180babab12e6b32c
Sneh Koul <snehkoul1999@gmail.com>
2019-04-15 12:36:29 -0700

    added post-message handler

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 
  if (typeof window.web3 !== "undefined") {
-  window.addEventListener('onboardingcomplete',function(event) {
-
-    var string = window.location.href
-    if( string.includes('fwd.metamask.io/')){
-      
-      var res = string.substring(17);
-      window.open(res);
-
-      window.close()
-    
-    }
-  
-
-     
-  },false);
+  window.addEventListener("message", receiveMessage, false)
   } else {
     window.location.reload()
   }
+
+
+  function receiveMessage(event)
+{
+  // Do we trust the sender of this message?  (might be
+  // different from what we originally opened, for example).
+  console.log(event.data)
+
+  // event.source is popup
+  // event.data is "hi there yourself!  the secret response is: rheeeeet!"
+}
