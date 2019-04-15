e379b6a63790becb181b71913ab21081a463177a
Sneh Koul <snehkoul1999@gmail.com>
2019-04-15 12:23:17 -0700

    checking for onboarding complete

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 
  if (typeof window.web3 !== "undefined") {
-  window.location = self.location;
-  location.reload( true ); 
-  } else {
+  window.addEventListener('onboardingcomplete',function(event) {
+
+    if( string.includes('fwd.metamask.io/')){
+      var string = window.location.href
+      var res = string.substring(17);
+      window.open(res);
+
+      window.close()
     
+    }
+  
+
+     
+  },false);
+  } else {
+    window.location.reload()
   }
